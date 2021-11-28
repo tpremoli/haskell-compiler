f0cb4ecc4390866e5e14dbb259b23f6c0a2d302a
tpremoli <75447700+tpremoli@users.noreply.github.com>
2021-11-28 21:31:08 +0000

    Implemented iexec for every instr

--- src/Machine.hs
+++ src/Machine.hs
@@ -38,9 +38,16 @@ type Stack = [Integer]
 -- Task 1.6 Define a type Config to model a configuration.
 type Config = (Integer, State, Stack)
 
---TODO Task 1.7 Define a function iexec :: Instr → Config → Config to execute a single instruction.
+-- Task 1.7 Define a function iexec :: Instr → Config → Config to execute a single instruction.
 iexec :: Instr -> Config -> Config
-iexec = undefined 
+iexec (LOADI x) (a,b,c)         = (a + 1, b, ((x):c))
+iexec (LOAD v) (a,b,c)          = let r = Data.Map.findWithDefault 0 v b
+                                in (a + 1, b, ((r):c))
+iexec (ADD) (a,b,c0:c1:c)       = (a + 1, b, ((c0+c1):c))
+iexec (STORE v) (a,b,c)         = (a + 1, (Data.Map.insert v (head c) b ), c)
+iexec (JMP i) (a,b,c)           = (a + i + 1, b, c)
+iexec (JMPLESS i) (a,b,c0:c1:c) = if(c1<c0) then (a + i + 1, b, c) else (a + 1, b, c)
+iexec (JMPGE i) (a,b,c0:c1:c)   = if(c1>=c0) then (a + i + 1, b, c) else (a + 1, b, c)
 
 --TODO Task 1.8 Define a function exec :: [ Instr ] → Config → Config to execute a list of instructions.
 exec :: [Instr] -> Config -> Config
