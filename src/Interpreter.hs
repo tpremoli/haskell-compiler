b75a4eb9981979d67f719d75f0b301ed63b72246
tpremoli <75447700+tpremoli@users.noreply.github.com>
2021-11-29 21:11:13 +0000

    Task 2.3: implemented bval for boolean evaluations.

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -28,12 +28,15 @@ data BExp = Bc Bool
         | Not Bool
         | And Bool Bool
         | Less AExp AExp
-    Bundefined
     deriving (Eq, Read, Show)
 
---TODO Task 2.3
+-- Task 2.3
 bval :: BExp -> State -> Bool
-bval = undefined 
+bval (Bc x) s               = x
+bval (Not x) s              = if x == True then False else True
+bval (And True True) s      = True
+bval (And _ _ ) s           = False
+bval (Less x y) s           = if (aval x s) < (aval y s) then True else False   
 
 -- Task 2.1
 data Com = Assign String AExp
