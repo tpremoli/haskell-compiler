e88cf17d72c0e2157a5cc6e16b8b0c51279f661b
tpremoli <75447700+tpremoli@users.noreply.github.com>
2021-11-29 22:26:18 +0000

    Restructured bval to work with BExp rather than Bool

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -25,18 +25,17 @@ aval (Plus x y) s           = (aval x s) + (aval y s)
 
 -- Task 2.1
 data BExp = Bc Bool 
-        | Not Bool
-        | And Bool Bool
+        | Not BExp
+        | And BExp BExp
         | Less AExp AExp
     deriving (Eq, Read, Show)
 
 -- Task 2.3
 bval :: BExp -> State -> Bool
-bval (Bc x) s               = x
-bval (Not x) s              = if x == True then False else True
-bval (And True True) s      = True
-bval (And _ _ ) s           = False
-bval (Less x y) s           = if (aval x s) < (aval y s) then True else False   
+bval (Bc x) s                       = x
+bval (Not x) s                      = if (bval x s) then False else True
+bval (And x y) s                    = if (bval x s) == (bval y s) && (bval y s) == True then True else False
+bval (Less x y) s                   = if (aval x s) < (aval y s) then True else False   
 
 -- Task 2.1
 data Com = Assign String AExp
