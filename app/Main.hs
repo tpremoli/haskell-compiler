2cd96a4dd1d739a094e3d174752cf98923fa2381
tpremoli <75447700+tpremoli@users.noreply.github.com>
2021-12-02 17:21:28 +0000

    Implemented Main method.Have to check if extra imports are allowed.

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,18 @@ module Main where
 
 import System.Environment
 import Compiler
+import Machine
+import Interpreter
+import Text.Read
 
---TODO Task 3.4
+-- Task 3.4
 main :: IO ()
-main = putStrLn "Implement"
+main = do
+    x <- getArgs
+    print (ccomp (readCom (x !! 0)))
+
+readCom :: String -> Com
+readCom s = read s
+
+-- example input:
+-- stack exec coursework-exe "Assign \"x\" (Plus (N 5) (N 3))"
