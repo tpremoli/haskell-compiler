99e20f4e31ddaf83724abe80ea5c3bf80a13105a
tpremoli <75447700+tpremoli@users.noreply.github.com>
2021-12-02 16:24:37 +0000

    implemented While statements in ccomp

--- src/Compiler.hs
+++ src/Compiler.hs
@@ -37,3 +37,8 @@ ccomp (If b x1 x2)          =   let i1 = ccomp x1   -- compile instruction 1
                                     i2 = ccomp x2   -- compile instruction 2
                                 in  -- Boolean if = True: do i1 then JMP i2. Boolean if = False: JMP i1 then do i2.
                                     (bcomp b False ((length i1)+1)) ++ i1 ++ [JMP (length i2)] ++ i2
+ccomp (While b x)           =   let i = ccomp x     -- compile the instruction
+                                    bc = bcomp b False (length (i) + 1) -- compile the boolean statement. 
+                                    -- Compiling with False means that when it's no longer true, it'll jump over i.
+                                in  -- If TRUE, won't jump over i, and instead will execute i and return to boolean statement.
+                                    bc ++ i ++ [JMP (- ((length i) + (length bc) + 1))] 
