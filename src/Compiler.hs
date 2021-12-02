e08ebe6127907fe61a76953e5f75658b80a8b814
tpremoli <75447700+tpremoli@users.noreply.github.com>
2021-12-02 16:15:39 +0000

    Beautified bcomp and added some comments

--- src/Compiler.hs
+++ src/Compiler.hs
@@ -14,19 +14,20 @@ acomp (N x)         =   [LOADI x]
 acomp (V x)         =   [LOAD x]
 acomp (Plus x y)    =   (acomp x) ++ (acomp y) ++ [ADD]
 
---TODO Task 3.2
+-- Task 3.2
 bcomp :: BExp -> Bool -> Int -> [Instr]
-bcomp (Bc x) y z            =   if x == y
+bcomp (Bc x) y z            =   if x == y       -- simple
                                     then [JMP z]
                                     else []
-bcomp (Not x) y z           =   if x /= (Bc y)
+bcomp (Not x) y z           =   if x /= (Bc y)  -- simple
                                     then [JMP z]
                                     else []
-bcomp (And x1 x2) y z       =   let r2 = bcomp x2 y z
-                                    r1 = bcomp x1 False (length (r2) + (if y == True then 0 else z))
-                                in r1 ++ r2
-bcomp (Less x1 x2) True z   =   (acomp x1) ++ (acomp x2) ++ [JMPLESS z]
-bcomp (Less x1 x2) False z  =   (acomp x1) ++ (acomp x2) ++ [JMPGE z]
+bcomp (And x1 x2) y z       =   let r2 = bcomp x2 y z   -- compile both statements.
+                                    r1 = bcomp x1 False (length (r2) + 
+                                            (if y == True then 0 else z))
+                                in r1 ++ r2 -- connect both statements
+bcomp (Less x1 x2) True z   =   (acomp x1) ++ (acomp x2) ++ [JMPLESS z] -- true meansJMPLSES
+bcomp (Less x1 x2) False z  =   (acomp x1) ++ (acomp x2) ++ [JMPGE z]   -- false means JMPGE
 
 --TODO Task 3.3
 ccomp :: Com -> [Instr]
